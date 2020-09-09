0f60f87e38f65de6bc6c08b52036544e74e6d960
Truong Cong Vinh <duongcongvinh18122001@gmail.com>
2020-09-09 13:52:58 +0700

    Assignment 1

---
+++ UWP/UWP/Model/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Model
+{
+    class Customer
+    {
+        private string fullname;
+        private string telephone;
+        private int age;
+        private DateTime birtday;
+
+        public string Fullname
+        {
+            get { return fullname; }
+            set { fullname = value; }
+
+        }
+        public string Telephone
+        {
+            get { return telephone; }
+            set { telephone = value; }
+
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+
+        }
+        public DateTime Birtday
+        {
+            get { return birtday; }
+            set { birtday = value; }
+
+        }
+
+        public Customer(string fullname, string telephone, int age, DateTime birtday)
+        {
+            Fullname = fullname;
+            Telephone = telephone;
+            Age = age;
+            Birtday = birtday;
+        }
+        public string toString()
+        {
+            return fullname + " | " +  telephone +" | "+ age + " | "+ birtday;
+        }
+    }
+}
---
+++ UWP/UWP/Pages/CusPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WPF.Model;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WPF.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CusPage : Page
+    {
+        public CusPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Customer c = new Customer(ten.Text, sdt.Text, Int32.Parse(tuoi.Text), ngaysinh.Date.DateTime.Date);
+            ListView.Items.Add(c.toString());
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
