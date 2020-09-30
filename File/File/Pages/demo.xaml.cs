0e3f458bf454c524d53d011bde0f50fda8320cba
Truong Cong Vinh <duongcongvinh18122001@gmail.com>
2020-09-30 16:41:36 +0700

    file

---
+++ File/File/Pages/demo.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using File.Services;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace File.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class demo : Page
+    {
+        public demo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileHandleService.WriteFile("uwpNoteFile.txt",txtContent.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            txtContent.Text = "";
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string txt = await FileHandleService.ReadFile("uwpNoteFile.txt");
+            FileHandleService.ExportFile("Filename",txt);
+        }
+    }
+}
---
+++ File/File/Services/FileHandleService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+namespace File.Services
+{
+    class FileHandleService
+    {
+        public static async Task<string> ReadFile(string fileName)
+        {
+            var storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            var demoFile = await storageFolder.CreateFileAsync(fileName, Windows.Storage.CreationCollisionOption.OpenIfExists);
+            return await FileIO.ReadTextAsync(demoFile);
+        }
+
+        public static async void WriteFile(string fileName, string content)
+        {
+            var storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            var demoFile = await storageFolder.CreateFileAsync(fileName, Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(demoFile, content);
+        }
+        public static async void ExportFile(string fileName, string content)
+        {
+            var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+            savePicker.SuggestedFileName = fileName;
+            var file = await savePicker.PickSaveFileAsync();
+
+            if (file != null)
+            {
+                FileIO.WriteTextAsync(file, content);
+            }
+        }
+    }
+}
